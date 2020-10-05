6fed6ffaffaaaf36952d607acf612ca6e8880ac0
Nataliia Kozlova <nataliia.k@sbtech.com>
2020-10-05 21:32:47 +0300

    Add project files.

---
+++ FruitTests/BaseFruitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit;
+using NUnit.Framework;
+
+namespace FruitTests
+{
+    [TestClass]
+    public class BaseFruitTests
+    {
+        [TestMethod]
+        public void GetFruitsPriceByWeight_10dollars_2kilos_return_20()
+        {
+        }
+    }
+}
