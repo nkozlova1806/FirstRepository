9db19034f581a6f7d762aea8ee3ae3faa66021dc
Nataliia K <kozlova.natalie.1987@gmail.com>
2020-10-07 20:56:49 +0300

    Revert "Revert "Add project files.""
    This reverts commit 003184a023173909177fe89489b0663dcad96882.

---
+++ Fruit/BasicStoreSet.cs
@@ -0,0 +1,25 @@
+﻿namespace Fruit
+{
+    public class BasicStoreSet
+    {
+        public BaseFruit Apple()
+        {
+            return new BaseFruit(14, 100, "apple", "green", 12);            
+        }
+
+        public BaseFruit Banana()
+        {
+            return new BaseFruit(9, 56, "banana", "yellow", 34);            
+        }
+
+        public BaseFruit Orange()
+        {
+            return new BaseFruit(7, 12, "orange", "orange", 57);            
+        }
+
+        public BaseFruit Cherry()
+        {
+            return new BaseFruit(12, 230, "cherry", "red",14);
+        }
+    }
+}
